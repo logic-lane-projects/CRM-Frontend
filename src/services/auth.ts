9646308d4460581d526c73631af7ad9fb9546907
RodrigoGutierrezPacheco <rodrigogutierrezpacheco@gmail.com>
2024-10-08 20:16:15 -0600

    agregando inicio de sesion con firebase

---
+++ src/services/auth.ts
@@ -0,0 +1,19 @@
+// src/services/auth.ts
+import { 
+  signInWithEmailAndPassword, 
+  signOut, 
+  createUserWithEmailAndPassword 
+} from 'firebase/auth';
+import { auth } from './../../firebase';
+
+export const login = (email: string, password: string) => {
+  return signInWithEmailAndPassword(auth, email, password);
+};
+
+export const logout = () => {
+  return signOut(auth);
+};
+
+export const register = (email: string, password: string) => {
+  return createUserWithEmailAndPassword(auth, email, password);
+};
