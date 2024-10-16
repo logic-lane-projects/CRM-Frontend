81d834de6ae6afe86baa724d0646a3681890f8b3
RodrigoGutierrezPacheco <rodrigogutierrezpacheco@gmail.com>
2024-10-15 21:13:15 -0600

    falta editar usuario

--- src/components/Modales/ModalRegistroLeads.tsx
+++ src/components/Modales/ModalRegistroLeads.tsx
@@ -1,235 +1,241 @@
 import {
-    Frame,
-    Modal,
-    TextContainer,
-    TextField,
-    Select,
-  } from "@shopify/polaris";
-  import { useState, useEffect } from "react";
-  import { createUserWithEmailAndPassword } from "firebase/auth";
-  import { auth } from "../../../firebase";
-  import { Toast } from "../Toast/toast";
-  import { UserRole } from "../../types/enums";
-  
-  // Declaración de la interfaz para las props del modal
-  interface ModalRegistroVendedoresProps {
-    isOpen: boolean;
-    setIsOpen: (value: boolean) => void;
-  }
-  
-  // Tipo para los valores del formulario
-  interface FormValues {
-    nombre: string;
-    apellidop: string;
-    apellidom: string;
-    correo: string;
-    contrasena: string;
-    confirmContrasena: string;
-    telefono: string;
-    ciudad: string;
-    rol: UserRole;
-  }
-  
-  // Inicialización de los valores del formulario
-  const initialFormValues: FormValues = {
-    nombre: "",
-    apellidop: "",
-    apellidom: "",
-    correo: "",
-    contrasena: "",
-    confirmContrasena: "",
-    telefono: "",
-    ciudad: "",
-    rol: UserRole.Vendedor,
-  };
-  
-  export default function ModalRegistroLeads({
-    isOpen,
-    setIsOpen,
-  }: ModalRegistroVendedoresProps) {
-    const [formValues, setFormValues] = useState<FormValues>(initialFormValues);
-    const [errors, setErrors] = useState<{ [key: string]: string }>({});
-    const [isSubmitDisabled, setIsSubmitDisabled] = useState(true);
-    const [isLoading, setIsLoading] = useState(false);
-    const [passwordsMatch, setPasswordsMatch] = useState(true);
-  
-    // Opciones para el select de roles, usando el enum
-    const roleOptions = [
-      { label: "Vendedor", value: UserRole.Vendedor },
-      { label: "Administrador", value: UserRole.Administrador },
-    ];
-  
-    // Manejador genérico de cambios en los campos del formulario
-    const handleFieldChange = (field: keyof FormValues, value: string) => {
-      setFormValues((prev) => ({ ...prev, [field]: value }));
-      setErrors((prev) => ({ ...prev, [field]: "" }));
-  
-      // Verifica si las contraseñas coinciden al cambiar cualquier campo de contraseña
-      if (field === "contrasena" || field === "confirmContrasena") {
-        setPasswordsMatch(
-          formValues.contrasena === value ||
-            formValues.confirmContrasena === value
-        );
-      }
-    };
+  Frame,
+  Modal,
+  TextContainer,
+  TextField,
+  Select,
+} from "@shopify/polaris";
+import { useState, useEffect } from "react";
+import { Toast } from "../Toast/toast";
+import { createLead } from "../../services/leads";
 
-    console.log(isSubmitDisabled)
-  
-    // Valida si todos los campos están completos y si las contraseñas coinciden para habilitar el botón
-    useEffect(() => {
-      const allFieldsFilled = Object.values(formValues).every(
-        (value) => value.trim() !== ""
-      );
-      const passwordsAreEqual =
-        formValues.contrasena === formValues.confirmContrasena;
-      setIsSubmitDisabled(!(allFieldsFilled && passwordsAreEqual));
-    }, [formValues]);
-  
-    const handleSubmit = async () => {
-      setIsLoading(true);
-      const newErrors: { [key: string]: string } = {};
-      Object.keys(formValues).forEach((key) => {
-        if (!formValues[key as keyof FormValues]) {
-          newErrors[key] = "Campo obligatorio";
-        }
-      });
-      if (formValues.contrasena !== formValues.confirmContrasena) {
-        newErrors.confirmContrasena = "Las contraseñas no coinciden";
-      }
-      if (Object.keys(newErrors).length > 0) {
-        setErrors(newErrors);
-        setIsLoading(false);
-        return;
-      }
-      setErrors({});
-      try {
-        const userCredential = await createUserWithEmailAndPassword(
-          auth,
-          formValues.correo,
-          formValues.contrasena
-        );
-        const user = userCredential.user;
-        console.log("Usuario registrado con éxito:", user);
-        Toast.fire({ icon: "success", title: "Usuario registrado" });
-        setIsOpen(false);
-      } catch (error) {
-        console.error("Error al registrar el usuario:", error);
-        setErrors((prev) => ({
-          ...prev,
-          correo: "Hubo un problema al registrar el usuario",
-        }));
-  
-        // Asegúrate de convertir el error a un string
-        const errorMessage = typeof error === "string" ? error : String(error);
-  
-        Toast.fire({
-          icon: "error",
-          title: errorMessage, 
-        });
-      } finally {
-        setIsLoading(false);
-      }
-    };
-  
-    return (
-      <div>
-        <Frame>
-          <Modal
-            open={isOpen}
-            onClose={() => setIsOpen(false)}
-            title="Registro de leads - en espera de modificaciones"
-            primaryAction={{
-              content: isLoading ? "Cargando..." : "Registrar",
-              onAction: handleSubmit,
-              disabled: true,
-            }}
-            secondaryActions={[
-              {
-                content: "Cancelar",
-                onAction: () => setIsOpen(false),
-              },
-            ]}
-          >
-            <Modal.Section>
-              <TextContainer>
-                <TextField
-                  label="Nombre"
-                  value={formValues.nombre}
-                  onChange={(value) => handleFieldChange("nombre", value)}
-                  autoComplete="off"
-                  error={errors.nombre}
-                />
-                <TextField
-                  label="Apellido Paterno"
-                  value={formValues.apellidop}
-                  onChange={(value) => handleFieldChange("apellidop", value)}
-                  autoComplete="off"
-                  error={errors.apellidop}
-                />
-                <TextField
-                  label="Apellido Materno"
-                  value={formValues.apellidom}
-                  onChange={(value) => handleFieldChange("apellidom", value)}
-                  autoComplete="off"
-                  error={errors.apellidom}
-                />
-                <TextField
-                  label="Correo electrónico"
-                  value={formValues.correo}
-                  onChange={(value) => handleFieldChange("correo", value)}
-                  autoComplete="off"
-                  error={errors.correo}
-                />
-                <TextField
-                  label="Contraseña"
-                  type="password"
-                  value={formValues.contrasena}
-                  onChange={(value) => handleFieldChange("contrasena", value)}
-                  autoComplete="off"
-                  error={errors.contrasena}
-                />
-                <TextField
-                  label="Confirmar Contraseña"
-                  type="password"
-                  value={formValues.confirmContrasena}
-                  onChange={(value) =>
-                    handleFieldChange("confirmContrasena", value)
-                  }
-                  autoComplete="off"
-                  error={
-                    !passwordsMatch
-                      ? "Las contraseñas no coinciden"
-                      : errors.confirmContrasena
-                  }
-                />
-                <TextField
-                  label="Teléfono"
-                  value={formValues.telefono}
-                  onChange={(value) => handleFieldChange("telefono", value)}
-                  autoComplete="off"
-                  error={errors.telefono}
-                />
-                <TextField
-                  label="Ciudad"
-                  value={formValues.ciudad}
-                  onChange={(value) => handleFieldChange("ciudad", value)}
-                  autoComplete="off"
-                  error={errors.ciudad}
-                />
-                <Select
-                  label="Rol"
-                  options={roleOptions}
-                  onChange={(value) =>
-                    handleFieldChange("rol", value as UserRole)
-                  }
-                  value={formValues.rol}
-                  error={errors.rol}
-                />
-              </TextContainer>
-            </Modal.Section>
-          </Modal>
-        </Frame>
-      </div>
+interface ModalRegistroVendedoresProps {
+  isOpen: boolean;
+  setIsOpen: (value: boolean) => void;
+}
+
+interface FormValues {
+  nombre: string;
+  apellidop: string;
+  apellidom: string;
+  correo: string;
+  telefono: string;
+  ciudad: string;
+  state: string | null;
+  birthday_date: string;
+  age: number;
+  type_lead: string;
+  gender: "MALE" | "FEMALE" | null;
+}
+
+const initialFormValues: FormValues = {
+  nombre: "",
+  apellidop: "",
+  apellidom: "",
+  correo: "",
+  telefono: "",
+  ciudad: "",
+  state: "",                 
+  birthday_date: "",          
+  age: 0,                     
+  type_lead: "TIBIO",          
+  gender: "MALE",              
+};
+
+export default function ModalRegistroLeads({
+  isOpen,
+  setIsOpen,
+}: ModalRegistroVendedoresProps) {
+  const [formValues, setFormValues] = useState<FormValues>(initialFormValues);
+  const [errors, setErrors] = useState<{ [key: string]: string }>({});
+  const [isSubmitDisabled, setIsSubmitDisabled] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const genderOptions = [
+    { label: "Selecciona una opción", value: "" },
+    { label: "Masculino", value: "MALE" },
+    { label: "Femenino", value: "FEMALE" },
+  ];
+
+  const typeLeadOptions = [
+    { label: "Tibio", value: "TIBIO" },
+    { label: "Frío", value: "FRIO" },
+    { label: "Caliente", value: "CALIENTE" },
+  ];
+
+  const handleFieldChange = (field: keyof FormValues, value: string | number) => {
+    setFormValues((prev) => ({ ...prev, [field]: value }));
+    setErrors((prev) => ({ ...prev, [field]: "" }));
+  };
+
+  useEffect(() => {
+    const allFieldsFilled = Object.values(formValues).every(
+      (value) => value !== "" && value !== null
     );
-  }
-  
+    setIsSubmitDisabled(!allFieldsFilled);
+  }, [formValues]);
+
+  const handleSubmit = async () => {
+    setIsLoading(true);
+    const newErrors: { [key: string]: string } = {};
+    Object.keys(formValues).forEach((key) => {
+      if (!formValues[key as keyof FormValues]) {
+        newErrors[key] = "Campo obligatorio";
+      }
+    });
+
+    if (Object.keys(newErrors).length > 0) {
+      setErrors(newErrors);
+      setIsLoading(false);
+      return;
+    }
+    setErrors({});
+
+    try {
+      const leadData = {
+        names: formValues.nombre,
+        paternal_surname: formValues.apellidop,
+        maternal_surname: formValues.apellidom,
+        email: formValues.correo,
+        phone_number: formValues.telefono,
+        city: formValues.ciudad,
+        state: formValues.state,        
+        type_lead: formValues.type_lead,       
+        gender: formValues.gender,              
+        is_client: false,                       
+        age: formValues.age,                   
+        birthday_date: formValues.birthday_date,
+      };
+
+      const leadResponse = await createLead(leadData);
+      console.log("Lead creado con éxito:", leadResponse);
+
+      Toast.fire({ icon: "success", title: "Lead registrado con éxito" });
+      setIsOpen(false);
+    } catch (error) {
+      console.error("Error al registrar el lead:", error);
+      setErrors((prev) => ({
+        ...prev,
+        correo: "Hubo un problema al registrar el lead",
+      }));
+
+      const errorMessage = typeof error === "string" ? error : String(error);
+
+      Toast.fire({
+        icon: "error",
+        title: errorMessage,
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <Frame>
+        <Modal
+          open={isOpen}
+          onClose={() => setIsOpen(false)}
+          title="Registro de Leads"
+          primaryAction={{
+            content: isLoading ? "Cargando..." : "Registrar",
+            onAction: handleSubmit,
+            disabled: isSubmitDisabled || isLoading,
+          }}
+          secondaryActions={[
+            {
+              content: "Cancelar",
+              onAction: () => setIsOpen(false),
+            },
+          ]}
+        >
+          <Modal.Section>
+            <TextContainer>
+              <TextField
+                label="Nombre"
+                value={formValues.nombre}
+                onChange={(value) => handleFieldChange("nombre", value)}
+                autoComplete="off"
+                error={errors.nombre}
+              />
+              <TextField
+                label="Apellido Paterno"
+                value={formValues.apellidop}
+                onChange={(value) => handleFieldChange("apellidop", value)}
+                autoComplete="off"
+                error={errors.apellidop}
+              />
+              <TextField
+                label="Apellido Materno"
+                value={formValues.apellidom}
+                onChange={(value) => handleFieldChange("apellidom", value)}
+                autoComplete="off"
+                error={errors.apellidom}
+              />
+              <TextField
+                label="Correo electrónico"
+                value={formValues.correo}
+                onChange={(value) => handleFieldChange("correo", value)}
+                autoComplete="off"
+                error={errors.correo}
+              />
+              <TextField
+                label="Teléfono"
+                value={formValues.telefono}
+                onChange={(value) => handleFieldChange("telefono", value)}
+                autoComplete="off"
+                error={errors.telefono}
+              />
+              <TextField
+                label="Ciudad"
+                value={formValues.ciudad}
+                onChange={(value) => handleFieldChange("ciudad", value)}
+                autoComplete="off"
+                error={errors.ciudad}
+              />
+              <TextField
+                label="Estado"
+                value={formValues.state || ""}
+                onChange={(value) => handleFieldChange("state", value)}
+                autoComplete="off"
+                error={errors.state}
+              />
+              <TextField
+                label="Fecha de Nacimiento"
+                type="date"
+                value={formValues.birthday_date || ""}
+                onChange={(value) => handleFieldChange("birthday_date", value)}
+                autoComplete="off"
+                error={errors.birthday_date}
+              />
+              <TextField
+                label="Edad"
+                type="number"
+                value={formValues.age.toString()}
+                onChange={(value) => handleFieldChange("age", Number(value))}
+                autoComplete="off"
+                error={errors.age}
+              />
+              <Select
+                label="Género"
+                options={genderOptions}
+                onChange={(value) =>
+                  handleFieldChange("gender", value as "MALE" | "FEMALE")
+                }
+                value={formValues.gender || ""}
+                error={errors.gender}
+              />
+              <Select
+                label="Tipo de Lead"
+                options={typeLeadOptions}
+                onChange={(value) => handleFieldChange("type_lead", value)}
+                value={formValues.type_lead || ""}
+                error={errors.type_lead}
+              />
+            </TextContainer>
+          </Modal.Section>
+        </Modal>
+      </Frame>
+    </div>
+  );
+}
---
+++ src/services/leads.ts
@@ -0,0 +1,199 @@
+// src/services/leads.ts
+export interface Lead {
+    _id?: string;
+    names: string;
+    paternal_surname: string;
+    maternal_surname: string;
+    email: string;
+    phone_number: string;
+    age: number;
+    birthday_date: string;
+    city: string | null;
+    state: string | null;
+    status?: string | null;
+    type_lead: string;
+    gender: "MALE" | "FEMALE" | null;
+    is_client: boolean | null;
+    created_at?: string;
+    updated_at?: string;
+  }
+  
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+// Crear un nuevo lead
+export const createLead = async (leadData: Lead): Promise<Lead> => {
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  const raw = JSON.stringify(leadData);
+
+  const requestOptions: RequestInit = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow" as RequestRedirect,
+  };
+
+  try {
+    const response = await fetch(`${API_URL}leads`, requestOptions);
+    if (!response.ok) {
+      throw new Error("Error al crear el lead");
+    }
+    const newLead: Lead = await response.json();
+    return newLead;
+  } catch (error) {
+    console.error("Error al crear un nuevo lead:", error);
+    throw error;
+  }
+};
+
+// Obtener un lead por ID
+export const getLeadById = async (id: string): Promise<Lead> => {
+    try {
+      const response = await fetch(`${API_URL}leads/${id}`, {
+        method: "GET",
+      });
+      if (!response.ok) {
+        throw new Error(`Error al obtener el lead con ID ${id}`);
+      }
+      
+      const data = await response.json(); 
+      return data.data; 
+    } catch (error) {
+      console.error(`Error al obtener el lead con ID ${id}:`, error);
+      throw error;
+    }
+  };
+  
+
+// Obtener un lead activo por ID
+export const getActiveLeadById = async (id: string): Promise<Lead> => {
+  try {
+    const response = await fetch(`${API_URL}leads/active/${id}`, {
+      method: "GET",
+    });
+    if (!response.ok) {
+      throw new Error(`Error al obtener el lead activo con ID ${id}`);
+    }
+    const lead: Lead = await response.json();
+    return lead;
+  } catch (error) {
+    console.error(`Error al obtener el lead activo con ID ${id}:`, error);
+    throw error;
+  }
+};
+
+// Obtener todos los leads (borrados y no borrados)
+export const getAllLeads = async (): Promise<Lead[]> => {
+  try {
+    const response = await fetch(`${API_URL}/leads`, {
+      method: "GET",
+    });
+    const data = await response.json();
+    return data.data; 
+  } catch (error) {
+    console.error("Error al obtener los leads:", error);
+    throw error;
+  }
+};
+
+// Obtener todos los leads activos
+export const getActiveLeads = async (): Promise<Lead[]> => {
+  try {
+    const response = await fetch(`${API_URL}leads/actives`, {
+      method: "GET",
+    });
+    if (!response.ok) {
+      throw new Error("Error al obtener los leads activos");
+    }
+    const leads: Lead[] = await response.json();
+    return leads;
+  } catch (error) {
+    console.error("Error al obtener los leads activos:", error);
+    throw error;
+  }
+};
+
+// Buscar leads activos por email
+export const getLeadsByEmail = async (email: string): Promise<Lead[]> => {
+  try {
+    const response = await fetch(`${API_URL}/leads/email/${email}`, {
+      method: "GET",
+    });
+    if (!response.ok) {
+      throw new Error(`Error al obtener los leads con email ${email}`);
+    }
+    const leads: Lead[] = await response.json();
+    return leads;
+  } catch (error) {
+    console.error(`Error al obtener los leads con email ${email}:`, error);
+    throw error;
+  }
+};
+
+// Buscar leads activos por teléfono
+export const getLeadsByPhoneNumber = async (
+  phoneNumber: string
+): Promise<Lead[]> => {
+  try {
+    const response = await fetch(
+      `${API_URL}/leads/phone_number/${phoneNumber}`,
+      {
+        method: "GET",
+      }
+    );
+    if (!response.ok) {
+      throw new Error(
+        `Error al obtener los leads con número de teléfono ${phoneNumber}`
+      );
+    }
+    const leads: Lead[] = await response.json();
+    return leads;
+  } catch (error) {
+    console.error(
+      `Error al obtener los leads con número de teléfono ${phoneNumber}:`,
+      error
+    );
+    throw error;
+  }
+};
+
+// Editar lead
+export const updateLead = async (id: string, leadData: Lead): Promise<Lead> => {
+  try {
+    const response = await fetch(`${API_URL}/leads/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(leadData),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error al actualizar el lead con ID ${id}`);
+    }
+
+    const updatedLead: Lead = await response.json();
+    return updatedLead;
+  } catch (error) {
+    console.error(`Error al actualizar el lead con ID ${id}:`, error);
+    throw error;
+  }
+};
+
+// Eliminar lead
+export const deleteLead = async (id: string): Promise<void> => {
+  try {
+    const response = await fetch(`${API_URL}leads/${id}`, {
+      method: "DELETE",
+    });
+    if (!response.ok) {
+      throw new Error(`Error al eliminar el lead con ID ${id}`);
+    }
+    console.log(`Lead con ID ${id} eliminado`);
+  } catch (error) {
+    console.error(`Error al eliminar el lead con ID ${id}:`, error);
+    throw error;
+  }
+};
